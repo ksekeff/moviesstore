f78390bf180fe858d57614f31f1f66a8fea6cd8a
Kalil Sekeff <ksekeff@gmail.com>
2025-01-18 16:19:58 -0300

    Add news css header

--- home/views.py
+++ home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'Cine Videos'
+    return render(request, 'home/index.html', {
+        'template_data': template_data
+    })
 
 def about(request):
+    template_data = {}
+    template_data['title'] = 'About'
     return render(request, 'home/about.html')
